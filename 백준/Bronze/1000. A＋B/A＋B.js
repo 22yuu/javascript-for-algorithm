48fcca54da23d39feac6a93491f0df6e6f58683f
22yuu <48381447+22yuu@users.noreply.github.com>
2023-03-22 20:35:47 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 9336 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+//input을 읽어와 변수로 선언 & 할당
+// 그 내용을 input에 저장, toString(), split()을 사용해서
+// Array로 저장된다.
+var input = fs.readFileSync('/dev/stdin').toString().split(' ');
+
+var a = parseInt(input[0]);
+var b = parseInt(input[1]);
+console.log(a+b);
