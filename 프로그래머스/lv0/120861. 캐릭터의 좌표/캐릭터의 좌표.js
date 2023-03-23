fe2530dd39456a2dbd76dec318219a39fbe1db5f
22yuu <48381447+22yuu@users.noreply.github.com>
2023-03-23 21:58:30 +0900

    [level 0] Title: 캐릭터의 좌표, Time: 0.09 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120861. 캐릭터의 좌표/캐릭터의 좌표.js
@@ -0,0 +1,28 @@
+function solution(keyinput, board) {
+    var answer = [0, 0];
+    const obj = {
+        up: [0, 1],
+        down: [0, -1],
+        left: [-1, 0],
+        right: [1, 0]
+    }
+    const minX = -Math.floor(board[0] / 2);
+    const maxX = Math.floor(board[0] / 2);
+    const minY = -Math.floor(board[1] / 2);
+    const maxY = Math.floor(board[1] / 2);
+    
+    for(let cmd of keyinput) {
+        const [x, y] = obj[cmd];
+        
+        const nx = answer[0] + x;
+        const ny = answer[1] + y;
+        
+        // 범위를 벗어날 경우 무시
+        if(nx < minX || ny < minY || nx > maxX || ny > maxY) continue;
+        
+        answer[0] = nx;
+        answer[1] = ny;
+    }
+    
+    return answer;
+}
