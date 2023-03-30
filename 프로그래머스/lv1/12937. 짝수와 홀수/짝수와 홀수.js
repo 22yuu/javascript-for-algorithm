5e30339c1d9d5cbb03624eed210a1f052fb192b2
22yuu <48381447+22yuu@users.noreply.github.com>
2023-03-30 22:36:56 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.02 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
+++ 프로그래머스/lv1/12937. 짝수와 홀수/짝수와 홀수.js
@@ -1,12 +1,13 @@
 function solution(num) {
     var answer = '';
     
-    if(num === 0) {
-        answer="Even";
-    } else if (num % 2 == 0) {
-        return answer="Even";
-    } else if (num % 2 == 1 || num % 2 == -1) {
-        return answer="Odd";
-    }
-    return answer;
+    // if(num === 0) {
+    //     answer="Even";
+    // } else if (num % 2 == 0) {
+    //     return answer="Even";
+    // } else if (num % 2 == 1 || num % 2 == -1) {
+    //     return answer="Odd";
+    // }
+    
+    return num % 2 === 0 ? "Even" : "Odd";
 }
