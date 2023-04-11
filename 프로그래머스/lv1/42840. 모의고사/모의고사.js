a72a4618ef29ef1dee2641d21506f37da5e4d0ea
22yuu <48381447+22yuu@users.noreply.github.com>
2023-04-11 22:22:09 +0900

    [level 1] Title: 모의고사, Time: 1.72 ms, Memory: 33.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/42840. 모의고사/모의고사.js
@@ -0,0 +1,39 @@
+function solution(answers) {
+    var answer = [];
+    const patterns = {
+        1: [1, 2, 3, 4, 5],
+        2: [2, 1, 2, 3, 2, 4, 2, 5],
+        3: [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
+    }
+    
+    const counts = {
+        1: 0,
+        2: 0,
+        3: 0
+    }
+    
+    for(let person = 1; person <= 3; person++) {
+        // console.log(patterns[person]);
+        const threshold = patterns[person].length;
+        let index = 0;
+
+        for(let i = 0; i < answers.length; i++) {
+            if(index === threshold) index = 0;
+            if(answers[i] === patterns[person][index++]) {
+                counts[person] += 1;
+            }
+        }
+    }
+    
+    const keys = Object.keys(counts);
+    const values = Object.values(counts);
+    const max = Math.max(...values);
+    
+    for(let [key, value] of Object.entries(counts)) {    
+        if(max === value) answer.push(Number(key));
+    }
+    
+    
+    
+    return answer.sort((a,b) => a - b);
+}
